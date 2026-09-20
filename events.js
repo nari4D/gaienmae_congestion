0eb9aeb3f0e8c99a425a1800cc25914cbbdcab09
nari4d <64998863+nari4D@users.noreply.github.com>
2026-09-20 18:00:04 +0900

    auto update

--- events.js
+++ events.js
@@ -1,6 +1,6 @@
-// 自動生成 — 2026/9/19 18:00:02
+// 自動生成 — 2026/9/20 18:00:04
 // 青年館は手動管理 (index.html の MANUAL_EVENTS)
-window.SCRAPED_EVENTS_UPDATED = "2026/9/19 18:00:02";
+window.SCRAPED_EVENTS_UPDATED = "2026/9/20 18:00:04";
 window.SCRAPED_EVENTS = [
   {
     "date": "2026-07-01",
@@ -828,6 +828,14 @@ window.SCRAPED_EVENTS = [
   },
   {
     "date": "2026-09-20",
+    "venue": "jingu",
+    "title": "東京六大学野球秋季リーグ戦",
+    "open": 10.5,
+    "start": 11,
+    "dur": 4
+  },
+  {
+    "date": "2026-09-21",
     "venue": "jingu",
     "title": "東京六大学野球秋季リーグ戦",
     "open": 10.5,
